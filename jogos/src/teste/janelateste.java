fb2d61d06ad8bd96a648811d8cd1502e93efabed
thaysferica <thaynara.couto.pereira@escola.pr.gov.br>
2025-09-30 09:52:26 -0300

    hu

---
+++ jogos/src/teste/janelateste.java
@@ -0,0 +1,13 @@
+package teste;
+
+import javax.swing.JFrame;
+
+public class janelateste {
+    public static void main(String[] args) {
+        JFrame  janela= new JFrame("titulo da janela");
+        janela.setBounds(0,0,1920,1080);
+        janela.setVisible(true);
+
+    }
+    
+}
